dda0a1c5eba022382e77e75d22e307acaa2332c8
lmy <myu.li.itlife@outlook.com>
2025-07-07 00:08:34 +1000

    go web基础 http 相关信息

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello World!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Snippet View!"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(http.StatusMethodNotAllowed)
+		//w.Write([]byte("Method not allowed"))
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Snippet Create!"))
+}
+
 func main() {
-	print("Hello World")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Printf("Starting server on port 8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
